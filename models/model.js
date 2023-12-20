927dc9c41d86da9dcc4f0343f0c35c6d91a6495a
zekhayoub <22371@ecam.be>
2023-12-20 04:40:20 +0100

    changement

--- models/model.js
+++ models/model.js
@@ -1,21 +1,43 @@
-const connection = require('./db.js');   // Attention ../db.js car on est dans models/model.js ---->CHANGEMENT de dossier = OK FAIT !
+const fs = require('fs');
 
-const getAllRooms = (callback) => {
-    connection.query("SELECT * FROM rooms;", callback);
-};
+// Read the content of pwd.txt synchronously
+try {
+    const passwordContent = fs.readFileSync('pwd.txt', 'utf8');
 
-const insertRoom = (roomName, roomLength, roomWidth, callback) => {
-    const insertQuery = 'INSERT INTO rooms (name, length, width) VALUES (?, ?, ?)';
-    connection.query(insertQuery, [roomName, roomLength, roomWidth], callback);
-};
+    const mysql = require("mysql");
 
-const deleteRoom = (roomId, callback) => {
-    const deleteQuery = 'DELETE FROM rooms WHERE id = ?';
-    connection.query(deleteQuery, [roomId], callback);
-};
+    // Connection à la base de données
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: passwordContent,
+        database: 'users'
+    });
 
-module.exports = {
-    getAllRooms,
-    insertRoom,
-    deleteRoom
-};
+    connection.connect(function (error) {
+        if (error) console.log(error);
+    });
+
+    // Fonctions liées aux requêtes à la base de données
+    const getAllRooms = (callback) => {
+        connection.query("SELECT * FROM rooms;", callback);
+    };
+
+    const insertRoom = (roomName, roomLength, roomWidth, callback) => {
+        const insertQuery = 'INSERT INTO rooms (name, length, width) VALUES (?, ?, ?)';
+        connection.query(insertQuery, [roomName, roomLength, roomWidth], callback);
+    };
+
+    const deleteRoom = (roomId, callback) => {
+        const deleteQuery = 'DELETE FROM rooms WHERE id = ?';
+        connection.query(deleteQuery, [roomId], callback);
+    };
+
+    module.exports = {
+        getAllRooms,
+        insertRoom,
+        deleteRoom
+    };
+} catch (err) {
+    console.error('Error reading pwd.txt:', err);
+}
